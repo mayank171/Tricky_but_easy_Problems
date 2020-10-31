ad8752a167c762ff5daed125b82ad1ac9d1be048
mayank <46345959+mayank171@users.noreply.github.com>
2020-10-31 10:43:37 +0530

    Initial Commit
    Divide a number by 4 without using '/' symbol.

---
+++ program_to_divide_number_by_4_without_using_divide_symbol.cpp
@@ -0,0 +1,13 @@
+// CPP program to divide a number by 4 
+// without using '/' 
+#include<iostream> 
+
+using namespace std; 
+int main() 
+{ 
+int n = 28; 
+n = n >> 2; 
+cout << n; 
+return 0; 
+} 
+
